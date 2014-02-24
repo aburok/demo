f3642d0a46744dbb03a4a7bba04a96d9c2d30890
aburok <gringo.kromka@gmail.com>
2014-02-24 11:39:38 +0100

    add csharp code katas

---
+++ katas/csharp/katas/katas/Calculator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace katas
+{
+    public class Calculator
+    {
+        public static int Add(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return 0;
+            }
+
+            var numbers = ParseInput(input);
+
+            var sum = numbers.Sum();
+
+            return sum;
+        }
+
+        private static IEnumerable<int> ParseInput(string input)
+        {
+            var delimiters = new List<char> {',', '\n'};
+            var unparsedNumbers = ParseCustomDelimiter(input, delimiters);
+
+            var numbers = ParseNumbers(unparsedNumbers, delimiters);
+
+            ValidateNumbers(numbers);
+
+            numbers = IgnoreNumbersOver1000(numbers);
+
+            return numbers; 
+        }
+
+        private static IEnumerable<int> ParseNumbers(string unparsedNumbers, List<char> delimiters)
+        {
+            var strNumbers = unparsedNumbers.Split(delimiters.ToArray());
+            var numbers = strNumbers.Select(int.Parse);
+            return numbers;
+        }
+
+        private static IEnumerable<int> IgnoreNumbersOver1000(IEnumerable<int> numbers)
+        {
+            numbers = numbers.Where(n => n <= 1000);
+            return numbers;
+        }
+
+        private static void ValidateNumbers(IEnumerable<int> numbers)
+        {
+            var negatives = numbers.Where(n => n < 0);
+            if (negatives.Any())
+            {
+                throw new ArgumentException(
+                    string.Format("negatives not allowed : {0}.", string.Join(",", negatives)));
+            }
+        }
+
+        private static string ParseCustomDelimiter(string input, List<char> delimiters)
+        {
+            var unparsedNumbers = input;
+
+            if (input.StartsWith("//"))
+            {
+                var custom = input.Substring(2).Substring(0, 1);
+                delimiters.Add(custom[0]);
+                unparsedNumbers = input.Substring(4);
+            }
+
+            return unparsedNumbers;
+        }
+    }
+}
---
+++ katas/csharp/katas/katas/Tests/Test_Calculator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace katas.Tests
+{
+    [TestFixture]
+    public class Test_Calculator
+    {
+        [Test]
+        public void Add_EmptyString_0()
+        {
+            //ACT
+            var sum = Calculator.Add("");
+
+            //ASSERT
+            Assert.AreEqual(0, sum);
+        }
+
+        [Test]
+        public void Add_SingleNumber_Correct()
+        {
+            //ACT
+            var sum = Calculator.Add("1");
+
+            Assert.AreEqual(1, sum);
+        }
+
+        [Test]
+        public void Add_TwoNumbers_Correct()
+        {
+            var sum = Calculator.Add("1,2");
+
+            Assert.AreEqual(3, sum);
+        }
+
+        [Test]
+        public void Add_UnknownAmountOfNumbers_Correct()
+        {
+            var sum = Calculator.Add("1,2,3,4,5,6,7,8,9,10,11,12");
+
+            Assert.AreEqual(78, sum);
+        }
+
+        [Test]
+        public void Add_EnableNewLineAsDelimiter_Correct()
+        {
+            var sum = Calculator.Add("1\n2,3");
+
+            Assert.AreEqual(6, sum);
+        }
+
+        [Test]
+        public void Add_SupportCustomDelimiters_Correct()
+        {
+            var sum = Calculator.Add("//;\n1,2\n3");
+
+            Assert.AreEqual(6, sum);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException), ExpectedMessage = "negatives not allowed : -1,-3.")]
+        public void Add_NegativeNumbersNotAllowd_ThrowException()
+        {
+            Calculator.Add("-1,2,-3\n4");
+        }
+
+        [Test]
+        [ExpectedException(typeof (ArgumentException), ExpectedMessage = "negatives not allowed : -1,-3.")]
+        public void AddWithCustomDelimiter_NegativeNumbersNotAllowed_ThrowException()
+        {
+            Calculator.Add("//;\n-1\n2;-3");
+        }
+
+        [Test]
+        public void Add_IgnorenumbersBiggerThan1000_Correct()
+        {
+            var sum = Calculator.Add("1,1000,1001");
+
+            Assert.AreEqual(1001, sum);
+        }
+    }
+}
