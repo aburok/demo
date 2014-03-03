5c44ab833adc486c3226cb3abfc2fa1331974be7
aburok <gringo.kromka@gmail.com>
2014-03-03 14:04:35 +0100

    add csharp training from pluralsight

---
+++ csharp/Training/clr/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using calc;
+
+namespace clr
+{
+    public class Class1
+    {
+        private static void Main()
+        {
+            //var url = "http://www.pluralsight.com";
+            //var uri = new Uri(url);
+
+            //Console.WriteLine(url.GetType().Assembly.FullName);
+            //Console.WriteLine(uri.GetType().Assembly.FullName);
+            //Console.WriteLine(typeof (Class1).Assembly.FullName);
+            //Console.WriteLine(typeof (Class1).AssemblyQualifiedName);
+            //Console.ReadLine();
+            Console.WriteLine(typeof(Calc).Assembly.CodeBase);
+
+            Console.ReadLine();
+        }
+    }
+}
